fc791d1bd9f848f7d90b4dd9b1828ab025f386fe
samosusa <samosusa123@gmail.com>
2024-03-20 14:21:09 +0100

    s

--- src/view/ChatTabsController.java
+++ src/view/ChatTabsController.java
@@ -10,13 +10,59 @@ import javafx.scene.layout.Region;
 import viewmodel.ChatTabsViewModel;
 public class ChatTabsController {
     @FXML
-private TextField globalMessage;
+    private TextField globalMessage;
     @FXML
     private ListView globalList;
     @FXML
-    private ListView onlineUserList;
+    private ListView onlineUsersList;
     @FXML
     private ListView messagesList;
-@FXML
-private TextField selected;
+    @FXML
+    private TextField selected;
+    @FXML
+    private TextField sendMessage;
+    @FXML
+    private Label errorLabel;
+
+
+private Region root;
+private ViewHandler viewHandler;
+private ChatTabsViewModel viewModel;
+
+    public void init(ViewHandler viewHandler, VinylLendingViewModel viewModel, Region root) {
+        this.viewHandler = viewHandler;
+        this.viewModel = viewModel;
+        this.root = root;
+        selected.textProperty().bind(viewModel.getSelected());
+        viewModel.loadMessages();
+        onlineUsersList.setItems(viewModel.getOnlineUsersList));
+        messagesList.setItems(viewModel.getMessagesList());
+        errorLabel.textProperty().bind(viewModel.getErrorLabel());
+        messagesList.setItems(viewModel.getMessagesList());
+
+        @FXML
+        private void onSend() {
+            viewModel.sendMessage(sendMessage.getText());
+            sendMessage.clear();
+        }
+
+        @FXML
+        private void onSelectUser(ActionEvent event) {
+            viewModel.getSelectedItem().set(userList.getSelectionModel().getSelectedItem());
+            viewModel.loadUsersMessage();
+        }
+        @FXML
+        private void onLogOut()
+        {
+            viewModel.logOut();
+            viewHandler.openLogin();
+        }
+        @FXML
+        private void onSendPrivateMessage(ActionEvent event){
+            viewModel.sendPrivate(sendPrivate.getText());
+
+        }
+
+
+    }
 }
